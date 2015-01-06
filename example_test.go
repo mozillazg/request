4033092345c1f8e3bf204409135fd53478c38afb
mozillazg <opensource.mozillazg@gmail.com>
2015-01-06 14:47:30 +0800

    testing

--- example_test.go
+++ example_test.go
@@ -1,1 +1,89 @@
 package request_test
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/mozillazg/request"
+)
+
+func ExampleGet() {
+	c := &http.Client{}
+	a := request.NewArgs(c)
+	url := "http://httpbin.org/get"
+	resp, _ := request.Get(url, a)
+	d, _ := resp.Json()
+	defer resp.Body.Close()
+	fmt.Println(resp.Ok())
+	fmt.Println(d.Get("url").MustString())
+	// Output:
+	//true
+	//http://httpbin.org/get
+}
+
+func ExampleGetparams() {
+	c := &http.Client{}
+	a := request.NewArgs(c)
+	a.Params = map[string]string{
+		"a": "1",
+		"b": "2",
+	}
+	url := "http://httpbin.org/get"
+	resp, _ := request.Get(url, a)
+	d, _ := resp.Json()
+	defer resp.Body.Close()
+	fmt.Println(d.Get("url").MustString())
+	fmt.Println(d.Get("args").MustMap())
+	// Output:
+	//http://httpbin.org/get?a=1&b=2
+	//map[a:1 b:2]
+}
+
+func ExamplePost() {
+	c := &http.Client{}
+	a := request.NewArgs(c)
+	a.Data = map[string]string{
+		"a": "1",
+		"b": "2",
+	}
+	url := "http://httpbin.org/post"
+	resp, _ := request.Post(url, a)
+	d, _ := resp.Json()
+	defer resp.Body.Close()
+	fmt.Println(resp.Ok())
+	fmt.Println(d.Get("form").MustMap())
+	// Output:
+	//true
+	//map[a:1 b:2]
+}
+
+func Examplecookies() {
+	c := &http.Client{}
+	a := request.NewArgs(c)
+	a.Cookies = map[string]string{
+		"name": "value",
+		"foo":  "bar",
+	}
+	url := "http://httpbin.org/cookies"
+	resp, _ := request.Get(url, a)
+	d, _ := resp.Json()
+	defer resp.Body.Close()
+	fmt.Println(resp.Ok())
+	fmt.Println(d.Get("cookies").MustMap())
+	// Output:
+	//true
+	//map[name:value foo:bar]
+}
+
+func Examplefiles() {
+	c := &http.Client{}
+	a := request.NewArgs(c)
+	f, _ := os.Open("test.txt")
+	a.Files = []request.FileField{
+		request.FileField{"abc", "abc.txt", f},
+	}
+	url := "http://httpbin.org/post"
+	resp, _ := request.Post(url, a)
+	defer resp.Body.Close()
+}
--- request_test.go
+++ request_test.go
@@ -1,1 +1,88 @@
 package request
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/bmizerany/assert"
+)
+
+func TestGet(t *testing.T) {
+	c := &http.Client{}
+	a := NewArgs(c)
+	url := "http://httpbin.org/get"
+	resp, _ := Get(url, a)
+	d, _ := resp.Json()
+	defer resp.Body.Close()
+
+	assert.Equal(t, resp.Ok(), true)
+	assert.Equal(t, d.Get("url").MustString(), url)
+}
+
+func TestHead(t *testing.T) {
+	c := &http.Client{}
+	a := NewArgs(c)
+	url := "http://httpbin.org/get"
+	resp, _ := Head(url, a)
+	defer resp.Body.Close()
+
+	assert.Equal(t, resp.Ok(), true)
+}
+
+func TestPost(t *testing.T) {
+	c := &http.Client{}
+	a := NewArgs(c)
+	url := "http://httpbin.org/post"
+	resp, _ := Post(url, a)
+	d, _ := resp.Json()
+	defer resp.Body.Close()
+
+	assert.Equal(t, resp.Ok(), true)
+	assert.Equal(t, d.Get("url").MustString(), url)
+}
+
+func TestPut(t *testing.T) {
+	c := &http.Client{}
+	a := NewArgs(c)
+	url := "http://httpbin.org/put"
+	resp, _ := Put(url, a)
+	d, _ := resp.Json()
+	defer resp.Body.Close()
+
+	assert.Equal(t, resp.Ok(), true)
+	assert.Equal(t, d.Get("url").MustString(), url)
+}
+
+func TestDelete(t *testing.T) {
+	c := &http.Client{}
+	a := NewArgs(c)
+	url := "http://httpbin.org/delete"
+	resp, _ := Delete(url, a)
+	d, _ := resp.Json()
+	defer resp.Body.Close()
+
+	assert.Equal(t, resp.Ok(), true)
+	assert.Equal(t, d.Get("url").MustString(), url)
+}
+
+func TestPatch(t *testing.T) {
+	c := &http.Client{}
+	a := NewArgs(c)
+	url := "http://httpbin.org/patch"
+	resp, _ := Patch(url, a)
+	d, _ := resp.Json()
+	defer resp.Body.Close()
+
+	assert.Equal(t, resp.Ok(), true)
+	assert.Equal(t, d.Get("url").MustString(), url)
+}
+
+func TestOptions(t *testing.T) {
+	c := &http.Client{}
+	a := NewArgs(c)
+	url := "http://httpbin.org/get"
+	resp, _ := Options(url, a)
+	defer resp.Body.Close()
+
+	assert.Equal(t, resp.Ok(), true)
+}

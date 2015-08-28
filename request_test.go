10525c743e77b7caf3cb211a1d1f8a65418bea23
mozillazg <opensource.mozillazg@gmail.com>
2015-08-28 13:09:44 +0800

    more tests

--- request_test.go
+++ request_test.go
@@ -163,6 +163,24 @@ func TestOptions(t *testing.T) {
 	assert.Equal(t, resp.Ok(), true)
 }
 
+func TestCookies(t *testing.T) {
+	c := new(http.Client)
+	req := NewRequest(c)
+	req.Cookies = map[string]string{
+		"key": "value",
+		"a":   "123",
+	}
+	resp, _ := req.Get("http://httpbin.org/cookies")
+	d, _ := resp.Json()
+	defer resp.Body.Close()
+
+	v := map[string]interface{}{
+		"key":   "value",
+		"a": "123",
+	}
+	assert.Equal(t, d.Get("cookies").MustMap(), v)
+}
+
 func TestPostJson(t *testing.T) {
 	c := new(http.Client)
 	req := NewRequest(c)
